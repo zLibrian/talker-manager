fb4d7b54ae8c311f08de5ff3e63302879efd93fb
Kevin <kevinwillian084@gmail.com>
2022-07-01 16:27:30 -0300

    fix(talkerRouter): a bug that kept the endpoint up after it had already ended

--- Routers/talkerRouter.js
+++ Routers/talkerRouter.js
@@ -11,13 +11,13 @@ talkerRouter.get('/:id', rescue((req, res, _next) => {
   const talkers = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
   const talker = talkers.find((t) => t.id === id);
   if (!talker) throw Error('TALKER_NOT_FOUND');
-  res.status(200).json(talker);
+  return res.status(200).json(talker);
 }));
 
 talkerRouter.get('/', rescue((req, res, _next) => {
   const filePath = path.resolve('./talker.json');
   const talkers = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
-  res.status(200).json(talkers);
+  return res.status(200).json(talkers);
 }));
 
 module.exports = talkerRouter;
