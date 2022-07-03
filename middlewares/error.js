c65f00eaf04b83fdf610f00f1b8ded39c09fc13b
Kevin <kevinwillian084@gmail.com>
2022-07-03 19:23:04 -0300

    feat(req 05): add 2 new error keys to errorMap

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,8 @@
 module.exports = (err, req, res, _next) => {
   const errorMap = {
     TALKER_NOT_FOUND: { code: 404, message: 'Pessoa palestrante não encontrada' },
+    TOKEN_NOT_FOUND: { code: 401, message: 'Token não encontrado' },
+    INVALID_TOKEN: { code: 401, message: 'Token inválido' },
   };
   const { code, message } = errorMap[err.message];
   return res.status(code).json({ message });
