7ef857a30bc0ee50f8d39ae60b6165100eb373ba
Kevin <kevinwillian084@gmail.com>
2022-07-03 20:22:42 -0300

    feat(req 05):  create POST endpoint to create a talker, and change a couple of things

--- Routers/talkerRouter.js
+++ Routers/talkerRouter.js
@@ -1,23 +1,34 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const rescue = require('express-rescue');
+const tokenValidator = require('../middlewares/tokenValidator');
+const { readAndConvertFileToJSON, convertAndWriteJsonToFile } = require('../helpers');
+const schemaValidator = require('../middlewares/schemaValidator');
+const talkerSchema = require('../schemas/talkerSchema');
 
 const talkerRouter = express.Router();
 
 talkerRouter.get('/:id', rescue((req, res, _next) => {
   const id = parseInt(req.params.id, 10);
-  const filePath = path.resolve('./talker.json');
-  const talkers = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+  const talkers = readAndConvertFileToJSON('./talker.json');
   const talker = talkers.find((t) => t.id === id);
   if (!talker) throw Error('TALKER_NOT_FOUND');
   return res.status(200).json(talker);
 }));
 
 talkerRouter.get('/', rescue((req, res, _next) => {
-  const filePath = path.resolve('./talker.json');
-  const talkers = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+  const talkers = readAndConvertFileToJSON('./talker.json');
   return res.status(200).json(talkers);
 }));
+
+talkerRouter.post('/', rescue(tokenValidator), rescue(schemaValidator(talkerSchema)),
+  rescue((req, res, _next) => {
+    const { name, age, talk } = req.body;
+    const talkers = readAndConvertFileToJSON('./talker.json');
+    const prevId = talkers[talkers.length - 1].id;
+    const talker = { id: prevId + 1, name, age, talk };
+    talkers.push(talker);
+    convertAndWriteJsonToFile('./talker.json', talkers);
+    return res.status(201).json(talker);
+  }));
 
 module.exports = talkerRouter;
